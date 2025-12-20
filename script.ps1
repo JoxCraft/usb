3cae664f34450537afa8fe8305983eab85af2766
JoxCraft <jakob.vonkapherr@gmail.com>
2025-12-20 22:58:15 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,2 +1,11 @@
-Start-Process "https://www.youtube.com"
-Read-Host "Press Enter to exit"
+# Create a background script block
+$scriptBlock = {
+    Start-Sleep -Seconds 10
+    Start-Process "https://www.youtube.com"
+}
+
+# Run in background job
+$job = Start-Job -ScriptBlock $scriptBlock
+
+# Exit immediately - job continues running
+Exit
