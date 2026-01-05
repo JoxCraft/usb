0822211dcf3f731a75511fd6672beb82361876fd
JoxCraft <jakob.vonkapherr@gmail.com>
2026-01-05 15:17:30 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -39,7 +39,7 @@ Start-Process -FilePath "powershell.exe" -WindowStyle Hidden -ArgumentList @(
     "-NoProfile",
     "-ExecutionPolicy", "Bypass",
     "-Command",
-    "Start-Sleep -Seconds 300; Start-Process 'msedge.exe' '--kiosk $kioskUrl --edge-kiosk-type=fullscreen --no-first-run'"
+    "Start-Sleep -Seconds 3; Start-Process 'msedge.exe' '--kiosk $kioskUrl --edge-kiosk-type=fullscreen --no-first-run'"
 )
 
 # Exit immediately
